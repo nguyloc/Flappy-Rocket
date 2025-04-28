bcb784ade893a9e9975134e0e25d954f08f5673a
Tan Loc <locnntps41055@gmail.com>
2025-04-28 22:35:10 +0700

    Add Rotation Input

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thurst;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thurstStrength = 100f;
     
     Rigidbody rb;
@@ -17,13 +18,26 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thurst.Enable();
+        rotation.Enable();
     }
 
     private void FixedUpdate()
+    {
+        ProcessThurst();
+        ProcessRotation();
+    }
+
+    private void ProcessThurst()
     {
         if (thurst.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thurstStrength * Time.fixedDeltaTime);
         }
+    }
+    
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        Debug.Log("Rotation Input: " + rotationInput);
     }
 }
