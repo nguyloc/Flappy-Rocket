1462f3115635545456c8e2d0779038cecd6934ae
Tan Loc <locnntps41055@gmail.com>
2025-04-28 22:11:05 +0700

    Using AddRelativeForce

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,17 +5,25 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thurst;
+    [SerializeField] float thurstStrength = 100f;
     
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void OnEnable()
     {
         thurst.Enable();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (thurst.IsPressed())
         {
-            Debug.Log("Thurst");
+            rb.AddRelativeForce(Vector3.up * thurstStrength * Time.fixedDeltaTime);
         }
     }
 }
